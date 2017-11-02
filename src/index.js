a19c2337d56e93746b62d05c2527a9c028fef62c
Pavel Tereschenko <me@pavlik.pro>
2017-11-02 03:55:53 +0300

    Add identity as default payload constructor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 type Action<T, P> = { +type: T, +payload: P }
-type EmptyAction<T> = { +type: T }
 type Reducer<S, A> = (state: S, action: A) => S
 type Handler<S> = {
   +reducer: Reducer<S, any>,
@@ -9,15 +8,6 @@ type Handler<S> = {
 }
 
 type CreateAction = (
-  <T: string>(type: T) => {
-    (): EmptyAction<T>,
-    +type: T,
-    +handle: <S>(reducer: Reducer<S, EmptyAction<T>>) => {
-      +reducer: Reducer<S, EmptyAction<T>>,
-      +type: T,
-    },
-  }
-) & (
   <T: string, D, P>(type: T, createPayload: (data: D) => P) => {
     (data: D): Action<T, P>,
     +type: T,
@@ -32,8 +22,10 @@ type CreateReducer =
   <S>(initialState: S, ...handlers: Handler<S>[]) =>
    <A>(state: S, action: A) => S
 
-export const createAction: CreateAction = ((type, createPayload) => {
-  const constructor = data => ({ type, payload: createPayload && createPayload(data) })
+const identity = value => value
+
+export const createAction: CreateAction = ((type, createPayload = identity) => {
+  const constructor = data => ({ type, payload: createPayload(data) })
   constructor.type = type
   constructor.handle = reducer => ({ reducer, type })
   return constructor
